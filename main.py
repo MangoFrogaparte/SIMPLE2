68c7f4bbf8222cfd59addf552d78e9f6f81e037a
MangoFrogaparte <devinbyers216@gmail.com>
2025-06-17 17:46:35 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4")
+response = llm.invoke("Why are we here?")
+print(response)
